cd4d7eb0645d7a6ea0fa00997cd8a961acf7ccd0
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-11-20 11:27:38 +0300

    created customer database model

---
+++ models/Customer.ts
@@ -0,0 +1,44 @@
+import mongoose, { Schema, Document, Model } from "mongoose";
+
+const CustomerSchema = new Schema(
+  {
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    salt: { type: String, required: true },
+    firstName: { type: String },
+    lastName: { type: String },
+    address: { type: String },
+    phone: { type: String, required: true },
+    verified: { type: Boolean },
+    otp: { type: String },
+    otp_expiry: { type: Date },
+    lat: { type: Number },
+    lng: { type: Number },
+    cart: [
+      {
+        food: { type: Schema.Types.ObjectId, ref: "food", required: true },
+        unit: { type: Number, required: true },
+      },
+    ],
+    orders: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "order",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      transform: (doc, ret) => {
+        delete ret.password;
+        delete ret.salt;
+        delete ret.__v;
+        delete ret.createdAt;
+        delete ret.updatedAt;
+      },
+    },
+    timestamps: true,
+  }
+);
+
+export const Customer = mongoose.model("customer", CustomerSchema);
