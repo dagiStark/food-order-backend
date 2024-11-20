dffa712fda62924dbe7c2aa4a1696b68caa7e600
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-11-20 15:42:03 +0300

    Update index.ts

---
+++ models/index.ts
@@ -0,0 +1,7 @@
+export * from "./Customer";
+export * from "./Vendor";
+export * from "./Order";
+export * from "./Food";
+export * from "./Offer";
+export * from "./DeliveryUser";
+export * from "./Transaction";
