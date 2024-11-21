005625df200ff491d9fa4ae4e1997ac378b1a286
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-11-21 13:08:36 +0300

    Update adminControllers.ts
    vendor creating function

--- controllers/adminControllers.ts
+++ controllers/adminControllers.ts
@@ -16,3 +16,51 @@ export const findVendor = async (id: string | undefined, email?: string) => {
   }
 };
 
+export const CreateVendor = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const {
+    name,
+    address,
+    pinCode,
+    foodType,
+    email,
+    phone,
+    password,
+    ownerName,
+  } = req.body;
+
+  const existingVendor = await Vendor.findOne({ email });
+  if (existingVendor) {
+    return res.status(400).json({ message: "Vendor already exists" });
+  }
+
+  const salt = generateSalt();
+  const hashedPassword = hashPassword(password, salt);
+
+  const vendor = new Vendor({
+    name,
+    address,
+    pinCode,
+    foodType,
+    email,
+    phone,
+    password: hashedPassword,
+    salt,
+    ownerName,
+    rating: 0,
+    serviceAvailable: false,
+    coverImages: [],
+    lat: 0,
+    lng: 0,
+  });
+  try {
+    await vendor.save();
+    res.status(201).json(vendor);
+  } catch (error) {
+    res.status(500).json({ message: "Error creating vendor" });
+  }
+};
+
