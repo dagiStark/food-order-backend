b66a84e7b0c4182d64bb1749376c484e3fd692fc
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-11-23 21:58:06 +0300

    Create shoppingController.ts
    create a function to check food availablity

---
+++ controllers/shoppingController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import { Offer, Vendor } from "../models";
+
+export const getFoodAvailability = async (req: Request, res: Response) => {
+  try {
+    const pinCode = req.params.pinCode;
+
+    const result = await Vendor.find({ pinCode, serviceAvailable: true })
+      .sort([["rating", "desc"]])
+      .populate("foods");
+
+    if (result.length > 0) {
+      return res.status(200).json({ data: result });
+    }
+
+    return res.status(400).json({ message: "No food available now!" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
