c1f1511c7ab488b0a61611c161f52ecfb82499ce
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-11-23 21:58:50 +0300

    Update shoppingController.ts
    create function to get top restaurants with their pin code

--- controllers/shoppingController.ts
+++ controllers/shoppingController.ts
@@ -19,3 +19,20 @@ export const getFoodAvailability = async (req: Request, res: Response) => {
   }
 };
 
+export const getTopRestaurants = async (req: Request, res: Response) => {
+  try {
+    const pinCode = req.params.pinCode;
+
+    const result = await Vendor.find({ pinCode, serviceAvailable: true })
+      .sort([["rating", "desc"]])
+      .limit(10);
+
+    if (result.length > 0) {
+      return res.status(200).json({ data: result });
+    }
+    return res.status(400).json({ message: "No Restaurants found!" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
